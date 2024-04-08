d5563da8e647c1953b0342dcc2d7df9111e5bc37
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-04-08 22:08:01 +0900

    Deleted Copilot enable and added keybinding for Copilot

--- init.lua
+++ init.lua
@@ -24,7 +24,7 @@ require('lazy').setup({
 	{ 'tpope/vim-repeat', keys = { { "U", mode = "n", }, }, },
 	'vim-jp/vimdoc-ja',
 	{ 'windwp/nvim-autopairs', config = true, keys = { { "U", mode = "n", }, }, },
-	{ 'github/copilot.vim', event = "VeryLazy" },
+	{ 'github/copilot.vim', keys = { { "C", mode = "c"} } },
 })
 
 local o = vim.o
@@ -215,7 +215,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 	callback = function()
 		vim.cmd[[r!git diff --cached]]
 		vim.cmd[[%s/^[^#]/#\0]]
-		vim.cmd[[Copilot enable]]
 	end
 })
 
