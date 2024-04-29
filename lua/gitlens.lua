e26ef1c6f293e8011041dc7c3656ef7db055ef7f
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-04-29 22:23:25 +0900

    Merged line

--- lua/gitlens.lua
+++ lua/gitlens.lua
@@ -10,12 +10,11 @@ function M.blameVirtText()
 	local line = api.nvim_win_get_cursor(0)
 	local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
 	local hash = vim.split(blame, '%s')[1]
-	local cmd = string.format("git show %s ", hash).."--format='%an | %ar | %s'"
 	if hash == '00000000' then
 		Text = 'Not Committed Yet'
 	else
-		Text = vim.fn.system(cmd)
-		Text = vim.split(Text, '\n')[1]
+		local cmd = string.format("git show %s ", hash).."--format='%an | %ar | %s'"
+		Text = vim.split(vim.fn.system(cmd), '\n')[1]
 		if Text:find("fatal") then
 			Text = 'Not Committed Yet'
 		end
