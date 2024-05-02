74888f0f6adeb338cdd9dcabb65d8cb8a1fced6e
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-02 17:01:34 +0900

    Enabled lazy load on map

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,10 +1,15 @@
-local map = vim.keymap.set
-
 vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
 
 require('lazy').setup({
 	{ 'folke/flash.nvim', config = true, event = "VimEnter" },
-	{ 'gbprod/substitute.nvim', opts = { modifiers = nil }, event = "VimEnter" },
+	{ 'gbprod/substitute.nvim', config = function()
+		local sub = require('substitute')
+		local map = vim.keymap.set
+		map('n', 's', sub.operator, { noremap = true })
+		map('n', 'ss', sub.line, { noremap = true })
+		map('n', 'S', sub.eol, { noremap = true })
+		map('x', 's', sub.visual, { noremap = true })
+	end, opts = { modifiers = nil }, event = "VimEnter" },
 	{ 'kylechui/nvim-surround', config = true, event = "VimEnter" },
 	{ 'smoka7/hop.nvim', config = true, event = "VimEnter" },
 	{ 'vim-jp/vimdoc-ja', keys = { { "h", mode = "c" } } },
@@ -13,10 +18,4 @@ require('lazy').setup({
 	{ 's417-lama/carbonpaper.vim', keys = { { "C", mode = "c" } } },
 	{ 'github/copilot.vim', keys = { { "C", mode = "c" } } },
 })
-
-local sub = require('substitute')
-map('n', 's', sub.operator, { noremap = true })
-map('n', 'ss', sub.line, { noremap = true })
-map('n', 'S', sub.eol, { noremap = true })
-map('x', 's', sub.visual, { noremap = true })
 
