4bc0c7c8f1c0f52896cc05431a4efc50bb42a8f0
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-05 22:16:40 +0900

    Added highlighting settings

--- init.lua
+++ init.lua
@@ -7,3 +7,9 @@ require('gitlens')
 vim.cmd.cabbrev { "nvimrc", "~/.config/nvim/init.lua" }
 
 vim.cmd.colorscheme'catppuccin'
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "Error", { bg = "none" })
+vim.api.nvim_set_hl(0, "ErrorMsg", { bg = "none" })
