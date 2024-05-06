d97da80e4c63c7d2f366fbdf2c89e7978fa436e3
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-06 12:38:18 +0900

    Rewrited fzf with lua

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -7,8 +7,4 @@ function FilesPicker(A, L, P)
 	end
 end
 
-function FilesRunner(args)
-	vim.cmd.edit(args)
-end
-
-vim.api.nvim_create_user_command("F", "lua FilesRunner(<q-args>)", { nargs = 1, bar = true, complete = "customlist,v:lua.FilesPicker" })
+vim.api.nvim_create_user_command("F", function(opts) vim.cmd.edit(opts.fargs[1]) end, { nargs = 1, bar = true, complete = "customlist,v:lua.FilesPicker" })
