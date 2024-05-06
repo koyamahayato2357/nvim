26437ab724b205f8fce99e6d5e215c215a826c6c
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-06 12:46:53 +0900

    Minimalized fzf

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -1,10 +1,12 @@
-function FilesPicker(A, L, P)
+vim.api.nvim_create_user_command("F",
+function(opts)
+	vim.cmd.edit(opts.fargs[1])
+end,
+{ nargs = 1, bar = true, complete = function(A)
 	local items = vim.split(vim.fn.system({ 'fd', '.', '-t', 'f' }), '\n')
 	if A:len() > 0 then
 		return vim.fn.matchfuzzy(items, A)
 	else
 		return items
 	end
-end
-
-vim.api.nvim_create_user_command("F", function(opts) vim.cmd.edit(opts.fargs[1]) end, { nargs = 1, bar = true, complete = "customlist,v:lua.FilesPicker" })
+end })
