0b0e267f388b4c8a1d92661f81a1fdfc30c399e7
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-12 15:19:10 +0900

    Deleted lazy.nvim

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,29 +1,63 @@
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+local au = vim.api.nvim_create_autocmd
+local map = vim.keymap.set
 
-require('lazy').setup({
-	{ 'folke/flash.nvim', config = function()
+local pluginpath = "/home/sundo/.local/share/nvim/plugins/"
+vim.opt.runtimepath:append(pluginpath .. 'flash.nvim')
+vim.opt.runtimepath:append(pluginpath .. 'substitute.nvim')
+vim.opt.runtimepath:append(pluginpath .. 'nvim-surround')
+vim.opt.runtimepath:append(pluginpath .. 'nvim')
+
+if vim.fn.isdirectory(vim.fn.expand('%')) == 1 then
+	vim.opt.runtimepath:append(pluginpath .. 'oil.nvim')
+	require'oil'.setup()
+	vim.opt.runtimepath:append(pluginpath .. 'nvim-web-devicons')
+else
+	au({ "CmdlineEnter" }, {
+		once = true,
+		callback = function()
+			vim.opt.runtimepath:append(pluginpath .. 'oil.nvim')
+			require'oil'.setup()
+			vim.opt.runtimepath:append(pluginpath .. 'nvim-web-devicons')
+		end
+	})
+end
+
+au({ "CmdlineEnter" }, {
+	once = true,
+	callback = function()
+		vim.opt.runtimepath:append(pluginpath .. 'vimdoc-ja')
+		vim.opt.runtimepath:append(pluginpath .. 'nvimdoc-ja')
+		vim.opt.runtimepath:append(pluginpath .. 'carbonpaper.vim')
+	end
+})
+
+au({ "InsertEnter" }, {
+	once = true,
+	callback = function()
+		vim.opt.runtimepath:append(pluginpath .. 'ultimate-autopair.nvim')
+		vim.opt.runtimepath:append(pluginpath .. 'copilot.nvim')
+		require'ultimate-autopair'.setup()
+	end
+})
+
+require'flash'.setup {
+	config = function()
 		local flash = require('flash')
-		local map = vim.keymap.set
 		map('n', 'gw', flash.remote)
 		map({ 'n', 'v', 'o' }, 'gs', flash.treesitter)
 		map({ 'n', 'v', 'o' }, 'gS', flash.treesitter_search)
-	end, event = "VeryLazy" },
-	{ 'gbprod/substitute.nvim', config = function()
-		local sub = require('substitute')
-		local map = vim.keymap.set
-		map('n', 's', sub.operator, { noremap = true })
-		map('n', 'ss', sub.line, { noremap = true })
-		map('n', 'S', sub.eol, { noremap = true })
-		map('x', 's', sub.visual, { noremap = true })
-	end, opts = { modifiers = nil }, keys = { { "s", mode = { "n", "v" } } } },
-	{ 'kylechui/nvim-surround', config = true, keys = { { "s", mode = "o" } } },
-	{ 'vim-jp/vimdoc-ja', keys = { { "h", mode = "c" } } },
-	{ 'vim-jp/nvimdoc-ja', keys = { { "h", mode = "c" } } },
-	{ "altermo/ultimate-autopair.nvim", branch = "v0.5", config = true, event = "InsertEnter" },
-	{ 's417-lama/carbonpaper.vim', keys = { { "C", mode = "c" } } },
-	{ 'stevearc/oil.nvim', config = true },
-	{ 'nvim-tree/nvim-web-devicons' },
-	{ 'catppuccin/nvim' },
-	{ 'github/copilot.vim', keys = { { "C", mode = "c" } } },
-})
+	end
+}
 
+require'substitute'.setup {
+	config = function()
+		local sub = require('substitute')
+		map('n', 's', sub.operator)
+		map('n', 'ss', sub.line)
+		map('n', 'S', sub.eol)
+		map('x', 's', sub.visual)
+	end,
+	opts = { modifiers = nil },
+}
+
+require'nvim-surround'.setup()
