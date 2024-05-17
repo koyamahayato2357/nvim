99fd4dfdd244b60af170c4b57019f2c02f264c19
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-17 16:11:54 +0900

    Refactored

--- syncplug.sh
+++ syncplug.sh
@@ -26,8 +26,8 @@ githuburl="https://github.com/"
 
 root=$(pwd)
 
-[ -d "$plugdir" ] || mkdir -p "$plugdir"
-cd "$plugdir"
+mkdir -p "$plugdir"
+cd "$plugdir" || exit
 
 for plugname in "${plugs[@]}"; do
 	reponame=$(basename "$plugname")
@@ -40,4 +40,4 @@ for plugname in "${plugs[@]}"; do
 	fi
 done
 
-cd "$root"
+cd "$root" || exit
