ca944013cfbcef36e665fcb1ce61613287aa3608
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-19 10:06:14 +0900

    Deleted else if and else because indentation break

--- lua/mysnip.lua
+++ lua/mysnip.lua
@@ -5,22 +5,16 @@ local map = vim.keymap.set
 if filetype == 'c' then
 	mysnip['inc'] = '#include <$1>\n$0'
 	mysnip['if'] = 'if ($1) {\n  $2\n}\n$0'
-	mysnip['eli'] = 'else if ($1) {\n$2\n}\n$0'
-	mysnip['els'] = 'else {\n$1\n}\n$0'
 	mysnip['for'] = 'for ($1; $2; $3) {\n  $4\n}\n$0'
 	mysnip['swi'] = 'switch ($1) {\n  $2\n}\n$0'
 	mysnip['cas'] = 'case $1:\n  $0'
 elseif filetype == 'lua' then
 	mysnip['if'] = 'if $1 then\n\t$2\nend\n$0'
-	mysnip['eli'] = 'elseif $1 then\n$0'
-	mysnip['els'] = 'else\n\t$0'
 	mysnip['for'] = 'for $1 in $2 do\n\t$3\nend\n$0'
 	mysnip['dfn'] = 'function $1($2)\n\t$3\nend\n$0'
 	mysnip['fun'] = 'function($1)\n\t$2\nend\n$0'
 elseif filetype == 'rust' then
 	mysnip['if'] = 'if $1 {\n\t$2\n}\n$0'
-	mysnip['eli'] = 'else if $1 {\n\t$2\n}\n$0'
-	mysnip['els'] = 'else {\n\t$1\n}\n$0'
 	mysnip['for'] = 'for $1 in $2 {\n\t$3\n}\n$0'
 	mysnip['whi'] = 'while $1 {\n\t$2\n}\n$0'
 	mysnip['mat'] = 'match $1 {\n\t$2\n}\n$0'
