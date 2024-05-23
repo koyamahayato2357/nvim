e70e26d4d4a42de75440dd7b4e4bafaa8635dc71
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-23 19:40:10 +0900

    NOW WORKS

--- lua/mydmacro.lua
+++ lua/mydmacro.lua
@@ -3,32 +3,21 @@ vim.b.keyhist = ''
 
 local function guess_macro()
 	local keyhist = vim.b.keyhist
-	-- for i = math.ceil(#keyhist / 2) + 1, #keyhist do
-	-- 	local curspan = keyhist:sub(i, #keyhist)
-	-- 	local cmpspan = keyhist:sub(#keyhist - 2 * i, i - 1)
-	-- 	if curspan == cmpspan then
-	-- 		vim.fn.feedkeys(curspan)
-	-- 		vim.b.prev_macro = curspan
-	-- 		return
-	-- 	end
-	-- end
 	for i = math.ceil(#keyhist / 2) + 1, #keyhist do
 		for j = 1, 2 * i - #keyhist do
 			local curspan = keyhist:sub(i, #keyhist)
 			local cmpspan = keyhist:sub(j, j + #keyhist - i)
 			if curspan == cmpspan then
 				local curmacro = keyhist:sub(j + #keyhist - i + 1, i - 1)
-				vim.fn.feedkeys(curmacro)
 				vim.b.prev_macro = curspan .. curmacro
-				vim.b.keyhist = vim.b.keyhist .. curmacro
-				return
+				return curmacro
 			end
 		end
 	end
 end
 
 local function record_macro(_, typed)
-	if vim.fn.keytrans(typed) ~= dmacro_key then
+	if typed ~= nil and typed ~= '' and vim.fn.keytrans(typed) ~= dmacro_key then
 		local mod = vim.fn.mode()
 		if mod ~= 'c' and not (mod == 'n' and typed == ':') then
 			vim.b.keyhist = vim.b.keyhist .. typed
@@ -42,7 +31,9 @@ end
 
 local function play_macro()
 	if vim.b.prev_macro == '' then
-		guess_macro()
+		local curmacro = guess_macro()
+		vim.fn.feedkeys(curmacro)
+		vim.b.keyhist = vim.b.keyhist .. curmacro
 	else
 		vim.fn.feedkeys(vim.b.prev_macro)
 	end
