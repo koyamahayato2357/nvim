f15420dc183c3e0b31402d5028cd3dfe1903febc
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-26 21:49:56 +0900

    Shortened snippets

--- lua/snippet.lua
+++ lua/snippet.lua
@@ -3,24 +3,24 @@ local filetype = vim.bo.filetype
 local map = vim.keymap.set
 
 if filetype == 'c' then
-	mysnip['inc'] = '#include <$1>\n$0'
-	mysnip['if'] = 'if ($1) {\n  $2\n}\n$0'
-	mysnip['for'] = 'for ($1; $2; $3) {\n  $4\n}\n$0'
-	mysnip['swi'] = 'switch ($1) {\n  $2\n}\n$0'
-	mysnip['cas'] = 'case $1:\n  $0'
+	mysnip['in'] = '#include <$1>\n$0'
+	mysnip['i'] = 'if ($1) {\n  $2\n}\n$0'
+	mysnip['f'] = 'for ($1; $2; $3) {\n  $4\n}\n$0'
+	mysnip['s'] = 'switch ($1) {\n  $2\n}\n$0'
+	mysnip['c'] = 'case $1:\n  $0'
 elseif filetype == 'lua' then
-	mysnip['if'] = 'if $1 then\n\t$2\nend\n$0'
-	mysnip['for'] = 'for $1 in $2 do\n\t$3\nend\n$0'
-	mysnip['dfn'] = 'function $1($2)\n\t$3\nend\n$0'
-	mysnip['fun'] = 'function($1)\n\t$2\nend\n$0'
+	mysnip['i'] = 'if $1 then\n\t$2\nend\n$0'
+	mysnip['f'] = 'for $1 in $2 do\n\t$3\nend\n$0'
+	mysnip['fn'] = 'function $1($2)\n\t$3\nend\n$0'
+	mysnip['l'] = 'function($1)\n\t$2\nend\n$0'
 elseif filetype == 'rust' then
-	mysnip['if'] = 'if $1 {\n\t$2\n}\n$0'
-	mysnip['for'] = 'for $1 in $2 {\n\t$3\n}\n$0'
-	mysnip['whi'] = 'while $1 {\n\t$2\n}\n$0'
-	mysnip['mat'] = 'match $1 {\n\t$2\n}\n$0'
-	mysnip['pat'] = '$1 => $2,\n$0'
-	mysnip['dfn'] = 'fn $1($2) {\n\t$3\n}\n$0'
-	mysnip['dfnr'] = 'fn $1($2) -> $3 {\n\t$4\n}\n$0'
+	mysnip['i'] = 'if $1 {\n\t$2\n}\n$0'
+	mysnip['f'] = 'for $1 in $2 {\n\t$3\n}\n$0'
+	mysnip['w'] = 'while $1 {\n\t$2\n}\n$0'
+	mysnip['m'] = 'match $1 {\n\t$2\n}\n$0'
+	mysnip['p'] = '$1 => $2,\n$0'
+	mysnip['fn'] = 'fn $1($2) {\n\t$3\n}\n$0'
+	mysnip['fnr'] = 'fn $1($2) -> $3 {\n\t$4\n}\n$0'
 elseif filetype == 'tex' then
 	mysnip['!'] = [[\documentclass[uplatex, dvipdfmx, a4paper]{bxjsarticle}
 \usepackage{}
@@ -33,10 +33,10 @@ elseif filetype == 'tex' then
 \newpage
 $0
 \end{document}]]
-	mysnip['beg'] = '\\begin{$1}\n$0'
-	mysnip['end'] = '\\end{$1}\n$0'
-	mysnip['sec'] = '\\section{$1}\n$0'
-	mysnip['sbs'] = '\\subsection{$1}\n$0'
+	mysnip['b'] = '\\begin{$1}\n$0'
+	mysnip['e'] = '\\end{$1}\n$0'
+	mysnip['s'] = '\\section{$1}\n$0'
+	mysnip['sb'] = '\\subsection{$1}\n$0'
 end
 
 map('n', 'sn', function()
