504426c6ca8aea6af678017c3ad7dd977c10d736
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-05-26 14:29:48 +0900

    Refactored dmacro.lua

--- lua/dmacro.lua
+++ lua/dmacro.lua
@@ -6,9 +6,9 @@ local function guess_macro()
 	for i = math.ceil(#keyhist / 2) + 1, #keyhist do
 		for j = 1, 2 * i - #keyhist do
 			local curspan = keyhist:sub(i, #keyhist)
-			local cmpspan = keyhist:sub(j, j + #keyhist - i)
+			local cmpspan = keyhist:sub(j, j + #curspan - 1)
 			if curspan == cmpspan then
-				local curmacro = keyhist:sub(j + #keyhist - i + 1, i - 1)
+				local curmacro = keyhist:sub(j + #curspan, i - 1)
 				vim.g.prev_macro = curspan .. curmacro
 				return curmacro
 			end
@@ -17,29 +17,13 @@ local function guess_macro()
 end
 
 local function record_macro(_, typed)
-	if typed == nil or typed == '' or vim.fn.keytrans(typed) == dmacro_key then
-		return
-	end
-	local mod = vim.fn.mode()
-	if mod == 'n' and typed == ':' or mod == 'c' then
-		return
-	end
-	vim.g.keyhist = vim.g.keyhist .. typed
-	vim.g.prev_macro = ''
-	if #vim.g.keyhist > 100 then
-		vim.g.keyhist = vim.g.keyhist:sub(-100)
-	end
-end
-
-local function play_macro()
-	if vim.g.prev_macro == '' then
-		local curmacro = guess_macro()
-		vim.fn.feedkeys(curmacro)
-		vim.g.keyhist = vim.g.keyhist .. curmacro
-	else
-		vim.fn.feedkeys(vim.g.prev_macro)
+	if typed and typed ~= '' and vim.fn.keytrans(typed) ~= dmacro_key then
+		vim.g.keyhist = vim.g.keyhist .. typed
+		vim.g.prev_macro = nil
 	end
 end
 
 vim.on_key(record_macro)
-vim.keymap.set({ 'i', 'n' }, dmacro_key, play_macro)
+vim.keymap.set({ 'i', 'n', 'v', 'o', 'c', 't' }, dmacro_key, function()
+	vim.fn.feedkeys(vim.g.prev_macro or guess_macro())
+end)
