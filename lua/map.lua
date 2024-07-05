ddd32d0fb3665dc391d1122d32df1b71d7a89005
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-07-05 18:40:48 +0900

    delete: nohl

--- lua/map.lua
+++ lua/map.lua
@@ -35,7 +35,6 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
 		map('n', '<CR>', '<Nop>')
 		map('n', 'n', 'nzzzv')
 		map('n', 'N', 'Nzzzv')
-		map('n', '<Esc>', vim.cmd.nohl)
 		map({ 'o', 'v' }, 'K', '5k')
 		map({ 'o', 'v' }, 'J', '5j')
 		map({ "n", "v", "o" }, "L", "5l")
