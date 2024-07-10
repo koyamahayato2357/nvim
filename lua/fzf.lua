36401971fade8bb7e13cf0386d280df005896fc8
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-07-10 11:43:18 +0900

    refactor

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -2,12 +2,12 @@
 ---@param files table
 ---@return table
 local function get_files_recursive(directory, files)
-	files = files
 	local curdir = vim.env.PWD
 	local directory_files = vim.fn.readdir(directory)
+	local ignored_dirs = { '.', '..', '.git', 'build', 'bin', 'plugins' }
 	for _, file in ipairs(directory_files) do
 		local full_path = directory .. '/' .. file
-		if vim.fn.isdirectory(full_path) ~= 0 and file ~= '.' and file ~= '..' and file ~= '.git' and file ~= 'build' and file ~= 'bin' and file ~= 'plugins' then
+		if vim.fn.isdirectory(full_path) ~= 0 and not vim.tbl_contains(ignored_dirs, file) then
 			get_files_recursive(full_path, files)
 		else
 			table.insert(files, full_path:sub(#curdir + 2))
@@ -15,6 +15,8 @@ local function get_files_recursive(directory, files)
 	end
 	return files
 end
+
+local file_cache = nil
 
 vim.api.nvim_create_user_command("F",
 	function(opts)
@@ -24,11 +26,13 @@ vim.api.nvim_create_user_command("F",
 		nargs = 1,
 		bar = true,
 		complete = function(trigger)
-			local items = get_files_recursive(vim.env.PWD, {})
+			if not file_cache then
+				file_cache = get_files_recursive(vim.env.PWD, {})
+			end
 			if #trigger > 0 then
-				return vim.fn.matchfuzzy(items, trigger)
+				return vim.fn.matchfuzzy(file_cache, trigger)
 			else
-				return items
+				return file_cache
 			end
 		end
 	})
--- lua/gitlens.lua
+++ lua/gitlens.lua
@@ -2,14 +2,10 @@ local map = vim.keymap.set
 local api = vim.api
 
 function PrintBlameText()
-	local ft = vim.fn.expand('%:h:t')
-	if ft == '' or ft == 'bin' then
-		return
-	end
 	local currFile = vim.fn.expand('%')
 	local line = api.nvim_win_get_cursor(0)
 	local blame = vim.fn.system(string.format('git blame -c -L %d,%d %s', line[1], line[1], currFile))
-	local hash = vim.split(blame, '%s')[1]
+    local hash = string.match(blame, '%S+')
 	if hash == '00000000' then
 		Text = 'Not Committed Yet'
 	else
