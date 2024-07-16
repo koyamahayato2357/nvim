2db61c33eb38bf5cbcb9ea5c2997e1ed1d780be5
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-07-17 06:02:26 +0900

    lazy load of myplugins

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 vim.opt.runtimepath = "~/.config/nvim,/usr/local/share/nvim/runtime,/usr/local/lib/nvim"
 
 require 'autocmd'
-require 'command'
-require 'fzf'
 require 'gitlens'
 require 'map'
 require 'opt'
