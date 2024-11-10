04dd5aaed8b51cdf9174ef04c95055f960283c61
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2024-11-10 18:11:37 +0900

    add: setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+./syncplug.sh
+cd plugins/blink.cmp
+cargo build --release
+nvim -c "lua Load_Plugin 'nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSInstall all"
+sudo cp -r plugins/nvim-treesitter/parser /usr/local/lib/nvim
+sudo cp -r plugins/nvim-treesitter/queries /usr/local/share/nvim/runtime
