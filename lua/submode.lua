2ebd6b3bd509107f3e840bdf1eecc53e57068f00
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2024-12-20 22:42:30 +0900

    enhanced submode

--- lua/submode.lua
+++ lua/submode.lua
@@ -6,33 +6,50 @@ local function make_uniq(str)
 	return '<Plug>(' .. str .. ')'
 end
 
+local function isfun(any)
+	return type(any) == 'function'
+end
+
+local function map(mode, lhs, rhs, remap)
+	vim.keymap.set(mode, lhs, rhs, { expr = isfun(rhs), remap = remap })
+end
+
 ---@class FolOpts
 
 ---@class Follower
 ---@field key string
----@field callback (fun(): string)?
+---@field callback fun(): string??
 ---@field opts FolOpts?
+
+---@class Autos
+---@field entering (fun(): string?|string)?
+---@field repeating (fun(): string?|string)?
+---@field leaving (fun(): string?|string)?
 
 ---@param mode string|string[]
 ---@param leader string
 ---@param followers Follower[]
-function M.set_submode_keymap(mode, leader, followers)
+---@param run_when Autos?
+function M.set_submode_keymap(mode, leader, followers, run_when)
+	run_when = run_when or {}
 	local uniqid = make_uniq(leader)
 	local uniqprefix = uniqid .. leader
-	vim.keymap.set(mode, uniqprefix, '', { remap = true })
 	for _, follower in ipairs(followers) do
 		local key = leader .. follower.key
 		local callback = follower.callback
-
-		if callback then
-			local cb = function() return (callback() or '') .. uniqprefix end
-			vim.keymap.set(mode, key, cb, { expr = true, remap = true })
-			vim.keymap.set(mode, uniqprefix .. follower.key, cb, { expr = true, remap = true })
-		else
-			vim.keymap.set(mode, key, key .. uniqprefix, { remap = true })
-			vim.keymap.set(mode, uniqprefix .. follower.key, key .. uniqprefix, { remap = true })
+		local rhs = not callback and key .. uniqprefix or function()
+			return (callback() or '') .. uniqprefix
 		end
+
+		run_when.entering = run_when.entering or rhs
+		run_when.repeating = run_when.repeating or rhs
+		run_when.leaving = run_when.leaving or ''
+
+		map(mode, key, run_when.entering, true)
+		map(mode, uniqprefix .. follower.key, run_when.repeating, true)
 	end
+
+	map(mode, uniqprefix, run_when.leaving, true)
 end
 
 ---@class AccOpts
@@ -41,8 +58,9 @@ end
 
 ---@param mode string|string[]
 ---@param key string
+---@param callback fun(): string??
 ---@param opts AccOpts?
-function M.acceleration_key(mode, key, opts)
+function M.acceleration_key(mode, key, callback, opts)
 	local uniqid = make_uniq(key)
 	local count ---@type number
 	local base = opts and opts.base or 2
@@ -54,12 +72,22 @@ function M.acceleration_key(mode, key, opts)
 		end
 
 		count = 1
+
+		if callback then
+			return (callback() or '') .. uniqid
+		end
+
 		return key .. uniqid
 	end, { expr = true })
 
 	vim.keymap.set(mode, uniqid .. key, function()
 		local speed = math.pow(base, math.floor(count / thre))
 		count = count + 1
+
+		if callback then
+			return speed .. (callback() or '') .. uniqid
+		end
+
 		return speed .. key .. uniqid
 	end, { expr = true })
 
