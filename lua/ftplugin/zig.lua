0c80b95525899003c38fe2bc6a6d166d432fc311
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2024-12-30 16:37:55 +0900

    add: zig snippet

--- lua/ftplugin/zig.lua
+++ lua/ftplugin/zig.lua
@@ -3,6 +3,15 @@ return {
 		name = 'zls',
 		cmd = { 'zls' }
 	},
+	snippet = {
+		{ abbr = 'i', snip = 'if ($1) {\n    $2\n}\n$0' },
+		{ abbr = 'f', snip = 'for ($1) |$2| {\n    $3\n}\n$0' },
+		{ abbr = 'w', snip = 'while ($1) {\n    $2\n}\n$0' },
+		{ abbr = 'fn', snip = 'fn $1($2) $3 {\n    $4\n}\n$0' },
+		{ abbr = 's', snip = 'switch ($1) {\n    $2\n}\n$0' },
+		{ abbr = 'p', snip = '$1 => $2,\n$0' },
+		{ abbr = 't', snip = 'test "$1" {\n    $2\n}' },
+	},
 	callback = function()
 		vim.bo.expandtab = true
 		vim.bo.shiftwidth = 4
