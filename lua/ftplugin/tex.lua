b7f1976fbf2b84a27b671e5b6582a77d70e1234a
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2024-12-30 16:38:13 +0900

    fix: tex snippet invalid escape

--- lua/ftplugin/tex.lua
+++ lua/ftplugin/tex.lua
@@ -6,20 +6,20 @@ return {
 	snippet = {
 		{
 			abbr = '!',
-			snip = [[\documentclass[uplatex, dvipdfmx, a4paper]{ltjsarticle}
-\usepackage{}
-\title{$1}
-\author{$2}
-\date{}
+			snip = [[\\documentclass[uplatex, dvipdfmx, a4paper]{ltjsarticle}
+\\usepackage{}
+\\title{$1}
+\\author{$2}
+\\date{}
 
-\begin{document}
-\maketitle
-\newpage
+\\begin{document}
+\\maketitle
+\\newpage
 $0
-\end{document}]]
+\\end{document}]]
 		},
-		{ abbr = 'b', snip = '\\begin{${1:env}}\n$2\n\\end{${1:env}\n$0}' },
-		{ abbr = 's', snip = '\\section{$1}\n$0' },
-		{ abbr = 'sb', snip = '\\subsection{$1}\n$0' },
+		{ abbr = 'b', snip = '\\\\begin{${1:env}}\n$2\n\\\\end{${1:env}}\n$0' },
+		{ abbr = 's', snip = '\\\\section{$1}\n$0' },
+		{ abbr = 'sb', snip = '\\\\subsection{$1}\n$0' },
 	}
 }
