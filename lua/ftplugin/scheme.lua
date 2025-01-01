de6913e5f2a222d10974a6903dd6d29eef31d13e
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-01-01 22:20:04 +0900

    add

--- lua/ftplugin/scheme.lua
+++ lua/ftplugin/scheme.lua
@@ -1,5 +1,9 @@
 return {
 	quickeval_cmd = 'scheme',
+	snippet = {
+		{ abbr = 'i', snip = '(if ${1:cond}\n  ${2:true}\n  ${3:false})' },
+		{ abbr = 'fn', snip = '(define $1 (lambda ($2)\n  $3))' },
+	},
 	callback = function()
 		vim.bo.expandtab = true
 		vim.bo.shiftwidth = 2
