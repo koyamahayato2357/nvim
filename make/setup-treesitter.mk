c0618a7793906a9b439a5a982853643192ec3c14
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-01-07 22:12:46 +0900

    fix: path

--- make/setup-treesitter.mk
+++ make/setup-treesitter.mk
@@ -3,8 +3,8 @@
 all: build install
 
 build:
-	nvim -c "lua Load_Plugin 'nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSInstall all"
+	nvim -c "lua Load_Plugin 'nvim-treesitter/nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSInstall all" +q
 
 install:
-	sudo cp -r plugins/nvim-treesitter/nvim-treesitter/parser /usr/local/lib/nvim
-	sudo cp -r plugins/nvim-treesitter/nvim-treesitter/queries /usr/local/share/nvim/runtime
+	cp -r plugins/nvim-treesitter/nvim-treesitter/parser ~/.local/lib/nvim
+	cp -r plugins/nvim-treesitter/nvim-treesitter/queries ~/.local/share/nvim/runtime
