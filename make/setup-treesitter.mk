f36dccf3cdb8ba88127a14332aeec10e0044839e
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-02-25 12:16:59 +0900

    fix: make treesitter

--- make/setup-treesitter.mk
+++ make/setup-treesitter.mk
@@ -3,7 +3,7 @@
 all: build install
 
 build:
-	nvim -c "lua Load_Plugin 'nvim-treesitter/nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSInstall all"
+	nvim -c "lua Load_Plugin 'nvim-treesitter/nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSUpdate all" -c "q"
 
 install:
 	cp -r plugins/nvim-treesitter/nvim-treesitter/parser ~/.local/lib/nvim
