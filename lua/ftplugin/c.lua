238a4b5c1156d7f44d75ff7407d6396e9b178add
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-03-05 09:57:09 +0900

    refactor: ftplugin

--- lua/ftplugin/c.lua
+++ lua/ftplugin/c.lua
@@ -1,28 +1,18 @@
-local lsp = {
-	name = 'clangd',
-	cmd = { 'clangd' }
-}
-
-local root = vim.fs.root(0, 'platformio.ini')
-if root then
-	table.insert(lsp[2], '--background-index')
-	table.insert(lsp[2],
-		'--query-driver=/home/sundo/.platformio/packages/toolchain-gccarmnoneeabi@1.90201.191206/bin/arm-none-eabi-g++')
-	lsp.root_dir = root
-end
-
 return {
-	lsp = lsp,
+	lsp = {
+		name = 'clangd',
+		cmd = { 'clangd' }
+	},
 	quickeval_cmd = 'clang-repl --summary-file=',
 	snippet = {
-		{ abbr = 'in', snip = '#include <$1.h>\n$0' },
-		{ abbr = 'i', snip = 'if ($1) {\n  $2\n}\n$0' },
-		{ abbr = 'f', snip = 'for ($1; $2; $3) {\n  $4\n}\n$0' },
-		{ abbr = 's', snip = 'switch ($1) {\n  $2\n}\n$0' },
-		{ abbr = 'c', snip = 'case $1:\n  $0' },
-		{ abbr = 'w', snip = 'while ($1) {\n  $2\n}\n$0' },
-		{ abbr = 'd', snip = '#define $0' },
-		{ abbr = 'p', snip = 'printf("$1", $2);\n$0' },
+		{ abbr = 'in',   snip = '#include <$1.h>\n$0' },
+		{ abbr = 'i',    snip = 'if ($1) {\n  $2\n}\n$0' },
+		{ abbr = 'f',    snip = 'for ($1; $2; $3) {\n  $4\n}\n$0' },
+		{ abbr = 's',    snip = 'switch ($1) {\n  $2\n}\n$0' },
+		{ abbr = 'c',    snip = 'case $1:\n  $0' },
+		{ abbr = 'w',    snip = 'while ($1) {\n  $2\n}\n$0' },
+		{ abbr = 'd',    snip = '#define $0' },
+		{ abbr = 'p',    snip = 'printf("$1", $2);\n$0' },
 		{ abbr = 'call', snip = '$1($2);\n$0' },
 	},
 	callback = function()
