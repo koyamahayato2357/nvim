8681cdbcc84e5a93afba48287b956ccb9b6d84f0
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-04-03 15:00:37 +0900

    refactor: rename

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -4,7 +4,7 @@ local map = vim.keymap.set
 local pluginOptions = require 'plugindata'
 
 ---@param plugname string
-function Load_Plugin(plugname)
+function Load_plugin(plugname)
 	if pluginOptions[plugname] then
 		local pluginpath = vim.fn.stdpath('config') .. "/plugins/"
 		local modname = pluginOptions[plugname].modname
@@ -13,7 +13,7 @@ function Load_Plugin(plugname)
 		local dependencies = pluginOptions[plugname].dependencies
 		if dependencies then
 			for _, dep in ipairs(dependencies) do
-				Load_Plugin(dep)
+				Load_plugin(dep)
 			end
 		end
 		vim.opt.runtimepath:append(pluginpath .. plugname)
@@ -25,30 +25,30 @@ end
 
 if vim.fn.argc() == 0 then
 	vim.cmd.rshada()
-	Load_Plugin 'stevearc/oil.nvim'
+	Load_plugin 'stevearc/oil.nvim'
 end
 
 au({ 'VimEnter' }, {
 	callback = function()
-		Load_Plugin 'folke/snacks.nvim'
+		Load_plugin 'folke/snacks.nvim'
 	end
 })
 
 au({ 'UIEnter' }, {
 	callback = function()
-		Load_Plugin 'folke/flash.nvim'
-		Load_Plugin 'gbprod/substitute.nvim'
-		Load_Plugin 'kylechui/nvim-surround'
-		Load_Plugin 'andersevenrud/nvim_context_vt'
+		Load_plugin 'folke/flash.nvim'
+		Load_plugin 'gbprod/substitute.nvim'
+		Load_plugin 'kylechui/nvim-surround'
+		Load_plugin 'andersevenrud/nvim_context_vt'
 	end
 })
 
 local function Load_cmdln()
 	require 'command'
 	require 'fzf'
-	Load_Plugin 'vim-jp/vimdoc-ja'
-	Load_Plugin 'vim-jp/nvimdoc-ja'
-	Load_Plugin 'stevearc/oil.nvim'
+	Load_plugin 'vim-jp/vimdoc-ja'
+	Load_plugin 'vim-jp/nvimdoc-ja'
+	Load_plugin 'stevearc/oil.nvim'
 end
 
 local cmdlnc = { ':', '/', '?' }
@@ -64,27 +64,27 @@ for _, c in ipairs(cmdlnc) do
 end
 
 map('n', '^a', function()
-	Load_Plugin 'zbirenbaum/copilot.lua'
+	Load_plugin 'zbirenbaum/copilot.lua'
 	local sug = require 'copilot.suggestion'
 	map('n', '^a', sug.toggle_auto_trigger)
 	sug.toggle_auto_trigger()
 end)
 
 map('n', 'g/', function()
-	Load_Plugin 'nvim-telescope/telescope.nvim'
+	Load_plugin 'nvim-telescope/telescope.nvim'
 	local tb = require 'telescope.builtin'
 	map('n', 'g/', tb.live_grep)
 	tb.live_grep()
 end)
 
 map('n', 'm', function()
-	Load_Plugin 'niuiic/track.nvim'
+	Load_plugin 'niuiic/track.nvim'
 	vim.keymap.del('n', 'm')
 	vim.fn.feedkeys 'm'
 end)
 
 map('n', '<A-n>', function()
-	Load_Plugin 'SmiteshP/nvim-navbuddy'
+	Load_plugin 'SmiteshP/nvim-navbuddy'
 	map('n', '<A-n>', vim.cmd.Navbuddy)
 	vim.cmd.Navbuddy()
 end)
@@ -92,19 +92,19 @@ end)
 au({ "InsertEnter", "CmdlineEnter" }, {
 	once = true,
 	callback = function()
-		if vim.bo.filetype ~= 'markdown' then Load_Plugin 'altermo/ultimate-autopair.nvim' end
-		Load_Plugin 'saghen/blink.cmp'
+		if vim.bo.filetype ~= 'markdown' then Load_plugin 'altermo/ultimate-autopair.nvim' end
+		Load_plugin 'saghen/blink.cmp'
 	end
 })
 
 au({ "CursorMoved" }, {
 	once = true,
 	callback = function()
-		Load_Plugin 'mini.indentscope'
-		Load_Plugin 'utilyre/sentiment.nvim'
-		Load_Plugin 'hadronized/hop.nvim'
-		if not vim.g.neovide and vim.env.TERM ~= 'xterm-kitty' then
-			Load_Plugin 'sphamba/smear-cursor.nvim'
-		end
+		Load_plugin 'mini.indentscope'
+		Load_plugin 'utilyre/sentiment.nvim'
+		Load_plugin 'hadronized/hop.nvim'
+		-- if not vim.g.neovide and vim.env.TERM ~= 'xterm-kitty' then
+		-- 	Load_Plugin 'sphamba/smear-cursor.nvim'
+		-- end
 	end
 })
