ce208070c5e26cc988049174bbf1b972981c7ff7
koyamahayato2357 <sundongxiaoshan202@gmial.com>
2025-04-04 20:03:46 +0900

    fix: TSUpdate

--- make/setup-treesitter.mk
+++ make/setup-treesitter.mk
@@ -5,7 +5,7 @@ PREFIX ?= ~/.local
 all: build install
 
 build:
-	nvim -c "lua Load_plugin 'nvim-treesitter/nvim-treesitter'" -c "lua require 'nvim-treesitter'" -c "TSUpdate all" -c "q"
+	nvim -c "lua Load_plugin 'nvim-treesitter/nvim-treesitter'" -c "TSUpdateSync all" -c "q"
 
 install:
 	cp -r plugins/nvim-treesitter/nvim-treesitter/parser $(PREFIX)/lib/nvim
